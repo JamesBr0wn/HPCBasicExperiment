9fdd65ac4f9cd504b9e83294e451cab0f25b6446
James Brown <wck593462925@outlook.com>
2018-11-27 09:43:28 +0800

    Changes.

--- Exp5/MatrixVectorMultiplication.c
+++ Exp5/MatrixVectorMultiplication.c
@@ -4,74 +4,90 @@
 
 int my_rank, comm_sz;
 
+// 获取矩阵和向量大小
 void get_size(int* m_ptr, int* local_m_ptr, int* n_ptr, int* local_n_ptr){
+    // 进程0读入矩阵和向量大小
     if(my_rank == 0){
         printf("Please enter the size of matrix and vector:\t");
         scanf("%d", n_ptr);
     }
+
+    // 将大小广播到所有进程
     MPI_Bcast(n_ptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // 计算线程局部矩阵、向量大小
     *m_ptr = *n_ptr;
     *local_m_ptr = *m_ptr / comm_sz;
     *local_n_ptr = *n_ptr / comm_sz;
 }
 
+// 为各进程局部运算数和结果分配内存空间
 void allocate_space(int m, int local_m, int local_n, double** local_mat_p, double** local_vec_p,  double** local_res_p){
     *local_mat_p = (double*)malloc(m * local_n * sizeof(double));
     *local_vec_p = (double*)malloc(local_n * sizeof(double));
     *local_res_p = (double*)malloc(local_m * sizeof(double));
 }
 
-void create_type(int m, int n, int local_n, MPI_Datatype* local_mat_t_ptr){
+// 为按列划分矩阵创建自定义类型
+d eate_type(int m, int n, int local_n, MPI_Datatype* local_mat_t_ptr){
     MPI_Datatype mpi_vec_t;
     MPI_Type_vector(m, local_n, n, MPI_DOUBLE, &mpi_vec_t);
     MPI_Type_create_resized(mpi_vec_t, 0, local_n * sizeof(double), local_mat_t_ptr);
     MPI_Type_commit(local_mat_t_ptr);
 }
 
-// Matrix shape: m * n
+// 读入矩阵
 void read_matrix(int m, int n, int local_n, double *local_mat, MPI_Datatype local_mat_t){
     double* matrix = NULL;
     int i;
 
-    if(my_rank == 0){
+    if(my_rank == 0){	// 进程0
+		// 读入矩阵
         matrix = (double*)malloc(m * n * sizeof(double));
         printf("Please enter the matrix:\n");
         for(i = 0; i < m * n; i++){
             scanf("%lf", matrix + i);
         }
-
+		
+		// 将属于各个进程的部分矩阵列散发出去
         MPI_Scatter(matrix, 1, local_mat_t, local_mat, m * local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         free(matrix);
-    }else{
+    }else{	// 其他进程
+		// 接收进程0散发的部分矩阵列
         MPI_Scatter(matrix, 1, local_mat_t, local_mat, m * local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 }
 
-// Vector shape: n * 1
+// 读入向量
 void read_vector(int n, int local_n, double* local_vec){
     double* vector = NULL;
     int i;
 
-    if(my_rank == 0){
+    if(my_rank == 0){	// 进程0
+		// 读入向量
         vector = (double*)malloc(n * sizeof(double));
         printf("Please enter the vector:\n");
         for(i  = 0; i < n; i++){
             scanf("%lf", vector + i);
         }
-
+		
+		// 将属于各个进程的部分向量散发出去
         MPI_Scatter(vector, local_n, MPI_DOUBLE, local_vec, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         free(vector);
-    }else{
+    }else{	// 其他进程
+		// 接收进程0散发的部分向量
         MPI_Scatter(vector, local_n, MPI_DOUBLE, local_vec, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 }
 
-void data_compute(int m, int local_m, int local_n, double *local_mat, double *local_vec, double *local_res){
+// 矩阵向量乘
+void data_compute(int m, int local_m, int local_n, const double *local_mat, const double *local_vec, double *local_res){
     int i, j, *recv_counts;
     double* median_res;
 
+	// 各线程计算自己的矩阵部分积
     median_res = (double*)malloc(m * sizeof(double));
     for(i = 0; i < m; i++){
         median_res[i] = 0;
@@ -80,26 +96,30 @@ void data_compute(int m, int local_m, int local_n, double *local_mat, double *lo
         }
     }
 
+	// 进行Reduce操作汇聚得到全局积并使用Scatter散发到各个进程
     recv_counts = (int*)malloc(comm_sz * sizeof(int));
     for(i = 0; i < comm_sz; i++){
         recv_counts[i] = local_m;
     }
-
     MPI_Reduce_scatter(median_res, local_res, recv_counts, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
     free(median_res);
     free(recv_counts);
 }
 
+// 打印矩阵
 void print_matrix(char* mat_name, int m, int n, int local_n, double* local_mat, MPI_Datatype local_mat_t){
     double* matrix = NULL;
     int i, j;
 
-    if(my_rank == 0){
+    if(my_rank == 0){	// 进程0
+		// 为整个矩阵分配完整空间
         matrix = (double*)malloc(m * n * sizeof(double));
-
+		
+		// 从其他进程聚集部分矩阵列得到完整矩阵
         MPI_Gather(local_mat, m * local_n, MPI_DOUBLE, matrix, 1, local_mat_t, 0, MPI_COMM_WORLD);
-
+		
+		// 打印出矩阵
         printf("%s\n", mat_name);
         for(i = 0; i < m; i++){
             for(j = 0; j < n; j++){
@@ -109,20 +129,25 @@ void print_matrix(char* mat_name, int m, int n, int local_n, double* local_mat, 
         }
 
         free(matrix);
-    }else{
+    }else{	// 其他进程
+		// 将部分矩阵列聚集到进程0
         MPI_Gather(local_mat, m * local_n, MPI_DOUBLE, matrix, 1, local_mat_t, 0, MPI_COMM_WORLD);
     }
 }
 
+// 打印向量
 void print_vector(char*vec_name, int n, int local_n, double* local_vec){
     double* vector = NULL;
     int i;
 
-    if(my_rank == 0){
+    if(my_rank == 0){	// 进程0
+		// 为整个向量分配完整空间
         vector = (double*)malloc(n * sizeof(double));
-
+		
+		// 从其他进程聚集部分向量得到完整向量
         MPI_Gather(local_vec, local_n, MPI_DOUBLE, vector, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+		
+		// 打印出向量
         printf("%s\n", vec_name);
         for(i = 0; i < n; i++){
             printf("%.3lf ", vector[i]);
@@ -131,6 +156,7 @@ void print_vector(char*vec_name, int n, int local_n, double* local_vec){
 
         free(vector);
     }else{
+		// 将部分向量聚集到进程0
         MPI_Gather(local_vec, local_n, MPI_DOUBLE, vector, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 }
@@ -161,7 +187,7 @@ int main(int argc, char** argv){
 
     data_compute(m, local_m, local_n, local_mat, local_vec, local_res);
 
-    print_vector("y=Ax", m, local_m, local_res);
+    print_vector("y = Ax", m, local_m, local_res);
 
     free(local_mat);
     free(local_vec);
